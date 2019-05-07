141f0ba6808b5202619c8767b2452bd9b33f6934
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-05-08 00:08:33 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main(){
-    FILE *file;
-    file = fopen("data", "r");
-    int datastorage [3][3];
-    int max;
-    int row, column;
+int main() {
+    float image[5][5][3];
+    float MAXr=0.000000, MAXb=0.000000, MAXg=0.000000, temp;
+    int x, y;
+    FILE *data;
+    data = fopen("data.txt", "w+");
     
-    for(int x=0; x<3; x++){
-        for(int i=0; i<3; i++){
-            int a;
-            fscanf(file, "%d", &a);
-            datastorage [x][i] = a;
+    srand(0);
+    
+    for(y = 0; y < 5; y++) {
+        for(x = 0; x < 5; x++) {
+            image[x][y][0] = (float)rand()/(float)INT_MAX;
+            image[x][y][1] = (float)rand()/(float)INT_MAX;
+            image[x][y][2] = (float)rand()/(float)INT_MAX;
         }
     }
     
-    for(int x=0; x<3; x++){
-        for(int i=0; i<3; i++){
-            if(datastorage[x][i]>max){
-                max = datastorage[x][i];
-                row = x;
-                column = i;
+    // ----------------------------------------
+    // Write your code below this comment block
+    // ----------------------------------------
+    
+    //printing all data to the screen
+    for(y = 0; y < 5; y++) {
+        for(x = 0; x < 5; x++) {
+            printf("red: %f blue: %f green: %f\n", image[x][y][0], image[x][y][1], image[x][y][2]);
+        }
+    }
+    
+    
+    for(y = 0; y < 5; y++) {
+        for(x = 0; x < 5; x++) {
+            fprintf(data, "%f\n%f\n%f\n", image[x][y][0], image[x][y][1], image[x][y][2]);
+        }
+    }
+    
+    rewind(data);
+    
+    for(y = 0; y < 5; y++) {
+        for(x = 0; x < 5; x++) {
+            fscanf(data, "%f", &temp);
+            if(temp>MAXr){
+                MAXr=temp;
+            }
+            fscanf(data, "%f", &temp);
+            if(temp>MAXb){
+                MAXb=temp;
+            }
+            fscanf(data, "%f", &temp);
+            if(temp>MAXg){
+                MAXg=temp;
             }
         }
     }
-    printf ("row: %d \ncolumn: %d\nmax int: %d", row, column, max);
+    
+    fclose(data);
+    
+    printf("maximum red: %f maximum blue: %f maximum green: %f\n", MAXr, MAXb, MAXg);
+    
     return 0;
 }
